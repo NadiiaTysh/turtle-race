9d54d9321c1b4b6be054a334024210e6f0567a0a
NadiiaTyshko <syao.xiwang@gmail.com>
2024-09-26 15:46:51 -0500

    Turtle race

--- main.py
+++ main.py
@@ -1,32 +1,35 @@
 from turtle import Turtle, Screen
+import  random
 
-tim = Turtle()
+is_race_on = False
 screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+turtles = {}
 
-def move_forwards():
-    tim.forward(10)
-
-def move_backwards():
-    tim.backward(10)
-
-def move_counter_clockwise():
-    heading = tim.heading()
-    tim.setheading(heading + 10)
-
-def move_clockwise():
-    heading = tim.heading()
-    tim.setheading(heading - 10)
-
-def clear_drawing():
-    tim.clear()
+def initiate_turtle(coloration, x, y):
+    tim = Turtle(shape="turtle")
+    tim.color(coloration)
     tim.penup()
-    tim.home()
-    tim.pendown()
+    tim.goto(x, y)
+    return tim
 
-screen.onkey(fun=move_forwards, key="w")
-screen.onkey(fun=move_backwards, key="s")
-screen.onkey(fun=move_counter_clockwise, key="a")
-screen.onkey(fun=move_clockwise, key="d")
-screen.onkey(fun=clear_drawing, key="c")
-screen.listen()
+for i, color in enumerate(colors):
+    turtles[color] = initiate_turtle(colors[i], -230, -90 + (i + 1) * 35)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle_color in turtles:
+        if turtles[turtle_color].xcor() > 230:
+            is_race_on = False
+            if turtle_color == user_bet:
+                print(f"You've won! The {turtle_color} turtle is the winner!")
+            else:
+                print(f"You've lost! The {turtle_color} turtle is the winner!")
+
+        turtles[turtle_color].forward(random.randint(0, 10))
+
 screen.exitonclick()
